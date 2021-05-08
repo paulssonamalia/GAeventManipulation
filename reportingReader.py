617b49dd63ea3bea27af25354e82b5f99d68d1b9
paulssonamalia <49182933+paulssonamalia@users.noreply.github.com>
2021-05-08 15:08:25 +0200

    Update reportingReader.py

--- reportingReader.py
+++ reportingReader.py
@@ -6,8 +6,10 @@ from statsmodels.tsa.stattools import adfuller
 import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense
-from datetime import date
+import datetime
 import logging
+import seaborn as sns
+import matplotlib.dates as md
 
 #Return errands within a session with time difference and number of clicks between subsequent 'Enter Profile'-events and 'Save Changes'-events.
 def getErrands(session):
@@ -15,36 +17,31 @@ def getErrands(session):
     startTime, endTime, clicks = 0, 0, 0
     date = None
     version = 'original'
-    lastTab = 'Landing page'
+    lastTab = 'Overview Click'
     changeSaved = False
     tabClicks = {'OverviewClick': 0, 'AddressClick': 0, 'ContactClick': 0, 'IndividualClick': 0, 'TransactionClick': 0, 'PurchasesClicks': 0, 'RewardsClicks': 0, 'OrdersClicks': 0, 'InteractionsClicks': 0, 'ProfileClicks': 0}
     clicksInTab = {'ClicksInOverview': 0, 'ClicksInAddress': 0, 'ClicksInContact': 0, 'ClicksInIndividual': 0, 'ClicksInTransaction': 0, 'ClicksInPurchases': 0, 'ClicksInRewards': 0, 'ClicksInOrders': 0, 'ClicksInInteractions': 0, 'ClicksInProfile': 0}
-    lastTabs = {'lastOverview': 0, 'lastAddress': 0, 'lastContact': 0, 'lastIndividual': 0, 'lastTransaction': 0, 'lastPurchases': 0, 'lastRewards': 0, 'lastOrders': 0, 'lastInteractions': 0, 'lastProfile': 0}
-    #set version to treatment
-    def setToTreatment():
-        nonlocal version
-        version = 'treatment'    
+    lastTabs = {'lastOverview': 0, 'lastAddress': 0, 'lastContact': 0, 'lastIndividual': 0, 'lastTransaction': 0, 'lastPurchases': 0, 'lastRewards': 0, 'lastOrders': 0, 'lastInteractions': 0, 'lastProfile': 0}   
+    #increase value to the key in tabClicks that match with action
     def increaseTabClicks(action):
-        if action not in ['Non Key Clicks', 'Enter Profile', 'Search Page', 'Reset Password', 'Save Changes']:
+        if action not in ['Non Key Clicks', 'Enter Profile', 'Search Page', 'Reset Password', 'Save Changes', 'stepInResult']:
             tabClicks[action.replace(' ', '')] += 1
-        if action in ['PurchasesClicks', 'RewardsClicks', 'OrdersClicks', 'InteractionsClicks', 'ProfileClicks']:
-            setToTreatment()
     #incrase click for action's respective tab 
     def increaseClicksInTab(action):
-        clicksInTab[action.replace(' ', '')] += 1
+        clicksInTab['ClicksIn' + action.split(' ')[0]] += 1
+
     session = session.sort_values(by=['Timestamp'])#Sort events after ascending timestamp
-    
     for index, row in session.iterrows(): #Iterate through every event within session
         date = row['Date Hour and Minute']
         if row['Event Action'] not in ['Non Key Clicks', 'Enter Profile', 'Search Page', 'Reset Password', 'Save Changes']:
             increaseTabClicks(row['Event Action'])
         if clicks and row['Event Action'] != 'Enter Profile' and not changeSaved:#increase number of clicks only between entering profile and saving change or within the same profile
             clicks +=1
-            if lastTab != 'Landing page' and row['Event Action'] == 'Non Key Clicks':
+            if row['Event Action'] == 'Non Key Clicks':
                 increaseClicksInTab(lastTab)
         if 'Click' in row['Event Action'] and not changeSaved and row['Event Action'] != 'Non Key Clicks':#Update latest tab
             lastTab = row['Event Action']
-        if 'Full URL' in row and '/start' in row['Full URL']:#if URL ends with start, the errand is in treatment
+        if 'Full URL' in row and '/start' in row['Full URL'] or row['Event Action'] in ['Purchases Clicks', 'Rewards Clicks', 'Orders Clicks', 'Interactions Clicks', 'Profile Clicks']:
             version = 'treatment'
         if row['Event Action'] == 'Enter Profile' or row['Event Action'] == 'Search Page' and row['Total Events'] == 1.0:
             if not clicks or clicks == 1:#if profile triggers for the first time or second in a row, an errand is initiated
@@ -53,16 +50,20 @@ def getErrands(session):
             elif clicks>1:#if profile triggers after other activities, the errand has ended
                 if not changeSaved:#if no changes have been made, endTime is updated
                     endTime = float(row['Timestamp'])
-                errands.append({'changeSaved': changeSaved, 'timeDiff': endTime-startTime, 'clicks': clicks, 'date': date, 'version': version, 'lastTab': lastTab,
+                lastTabs['last' + lastTab.split(' ')[0]] = 1#TODO look after different lastTab - Landing Page
+                errands.append({'changeSaved': changeSaved, 'timeDiff': endTime-startTime, 'clicks': float(clicks), 'date': date, 'version': version, 'lastTab': lastTab,
                 'OverviewClicks': tabClicks['OverviewClick'], 'AddressClicks': tabClicks['AddressClick'], 'ContactClicks': tabClicks['ContactClick'], 'IndividualClicks': tabClicks['IndividualClick'], 
                 'TransactionClicks': tabClicks['TransactionClick'], 'PurchasesClicks': tabClicks['PurchasesClicks'], 'RewardsClicks': tabClicks['RewardsClicks'], 'OrdersClicks': tabClicks['OrdersClicks'], 
                 'InteractionsClicks': tabClicks['InteractionsClicks'], 'ProfileClicks': tabClicks['ProfileClicks'], 'ClicksInOverview': clicksInTab['ClicksInOverview'], 'ClicksInAddress': clicksInTab['ClicksInAddress'], 
                 'ClicksInContact': clicksInTab['ClicksInContact'], 'ClicksInIndividual': clicksInTab['ClicksInIndividual'], 'ClicksInTransaction': clicksInTab['ClicksInTransaction'], 'ClicksInPurchases': clicksInTab['ClicksInPurchases'], 
-                'ClicksInRewards': clicksInTab['ClicksInRewards'], 'ClicksInOrders': clicksInTab['ClicksInOrders'], 'ClicksInInteractions': clicksInTab['ClicksInInteractions'], 'ClicksInProfile': clicksInTab['ClicksInProfile']})
+                'ClicksInRewards': clicksInTab['ClicksInRewards'], 'ClicksInOrders': clicksInTab['ClicksInOrders'], 'ClicksInInteractions': clicksInTab['ClicksInInteractions'], 'ClicksInProfile': clicksInTab['ClicksInProfile'],
+                'lastOverview': lastTabs['lastOverview'], 'lastAddress': lastTabs['lastAddress'], 'lastContact': lastTabs['lastContact'], 'lastIndividual': lastTabs['lastIndividual'], 'lastTransaction': lastTabs['lastTransaction'], 
+                'lastPurchases': lastTabs['lastPurchases'], 'lastRewards': lastTabs['lastRewards'], 'lastOrders': lastTabs['lastOrders'], 'lastInteractions': lastTabs['lastInteractions'], 'lastProfile': lastTabs['lastProfile']})
                 startTime, endTime, clicks = 0, 0, 0
                 changeSaved = False
-                tabClicks = {'OverviewClick': 0, 'AddressClick': 0, 'ContactClick': 0, 'IndividualClick': 0, 'TransactionClick': 0, 'PurchasesClicks': 0, 'RewardsClicks': 0, 'OrdersClicks': 0, 'InteractionsClicks': 0, 'ProfileClicks': 0}
-                clicksInTab = {'ClicksInOverview': 0, 'ClicksInAddress': 0, 'ClicksInContact': 0, 'ClicksInIndividual': 0, 'ClicksInTransaction': 0, 'ClicksInPurchases': 0, 'ClicksInRewards': 0, 'ClicksInOrders': 0, 'ClicksInInteractions': 0, 'ClicksInProfile': 0}
+                lastTabs['last' + lastTab.split(' ')[0]] = 0
+                tabClicks = dict.fromkeys(tabClicks, 0)
+                clicksInTab = dict.fromkeys(clicksInTab, 0)
         elif row['Event Action'] == 'Save Changes' and startTime and not endTime and row['Total Events'] == 1.0:
             endTime = float(row['Timestamp'])
             changeSaved = True
@@ -97,20 +98,17 @@ def forecastULeffect(d):
 #Return a dataFrame populated with sessionID, timeDiff, clicks, date and version.
 def populateDataFrames():
     df = pd.read_csv('reporting.csv')
-    d1 = pd.DataFrame(columns = ['changeSaved', 'timeDiff', 'clicks', 
-                    'date', 'version', 'lastTab', 'OverviewClicks', 'AddressClicks', 
-                    'ContactClicks', 'IndividualClicks', 'TransactionClicks', 'PurchasesClicks', 
-                    'RewardsClicks', 'OrdersClicks', 'InteractionsClicks', 'ProfileClicks', 
-                    'ClicksInOverview', 'ClicksInAddress', 'ClicksInContact', 'ClicksInIndividual', 
-                    'ClicksInTransaction', 'ClicksInPurchases', 'ClicksInRewards', 'ClicksInOrders', 
-                    'ClicksInInteractions', 'ClicksInProfile'])
-    d2 = pd.DataFrame(columns = ['changeSaved', 'timeDiff', 'clicks', 
-                    'date', 'version', 'lastTab', 'OverviewClicks', 'AddressClicks', 
-                    'ContactClicks', 'IndividualClicks', 'TransactionClicks', 'PurchasesClicks', 
-                    'RewardsClicks', 'OrdersClicks', 'InteractionsClicks', 'ProfileClicks', 
-                    'ClicksInOverview', 'ClicksInAddress', 'ClicksInContact', 'ClicksInIndividual', 
-                    'ClicksInTransaction', 'ClicksInPurchases', 'ClicksInRewards', 'ClicksInOrders', 
-                    'ClicksInInteractions', 'ClicksInProfile'])
+    columns = ['changeSaved', 'timeDiff', 'clicks',
+                'date', 'version', 'lastTab', 'OverviewClicks', 'AddressClicks', 
+                'ContactClicks', 'IndividualClicks', 'TransactionClicks', 'PurchasesClicks', 
+                'RewardsClicks', 'OrdersClicks', 'InteractionsClicks', 'ProfileClicks', 
+                'ClicksInOverview', 'ClicksInAddress', 'ClicksInContact', 'ClicksInIndividual', 
+                'ClicksInTransaction', 'ClicksInPurchases', 'ClicksInRewards', 'ClicksInOrders', 
+                'ClicksInInteractions', 'ClicksInProfile', 'lastOverview', 'lastAddress', 
+                'lastContact', 'lastIndividual', 'lastTransaction', 'lastPurchases', 'lastRewards', 
+                'lastOrders', 'lastInteractions', 'lastProfile']
+    d1 = pd.DataFrame(columns = columns)
+    d2 = pd.DataFrame(columns = columns)
     for sessionID in df['SessionID'].unique():
         errands = getErrands(df[df['SessionID'].str.contains(sessionID)])
         if errands == []:
@@ -122,34 +120,8 @@ def populateDataFrames():
                 d2 = d2.append(errand, ignore_index=True)
     return d1, d2   
 
-#Return dataFrame with mean, count, std and 95% confidence intervall for pairs of units and dataframes in dict.
+#Return dataFrame with mean, count, std and 90% confidence intervall for pairs of units and dataframes in dict.
 def getStats(dict):
-    #return the frequence of which each tabs were lastly used in an errand
-    def getLastTabDistribution(lastTab):
-        overview, address, contact, individual, transaction, purchases, rewards, orders, interactions, profile = 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-        if lastTab == 'Overview Click':
-            overview = 1
-        elif lastTab == 'Address Click':
-            address = 1
-        elif lastTab == 'Contact Click':
-            contact = 1
-        elif lastTab == 'Individual Click':
-            individual = 1
-        elif lastTab == 'Transaction Click':
-            transaction = 1
-        elif lastTab == 'Purchases Clicks':
-            purchases = 1
-        elif lastTab == 'Rewards Clicks':
-            rewards = 1
-        elif lastTab == 'Orders Clicks':
-            orders = 1 
-        elif lastTab == 'Interactions Clicks':
-            interactions = 1
-        elif lastTab == 'Profile Clicks':
-            profile = 1
-        return {'Overview': overview, 'Address': address, 'Contact': contact, 'Individual': individual, 'Transaction': transaction, 
-        'Purchases': purchases, 'Rewards': rewards, 'Orders': orders, 'Interactions': interactions, 'Profile': profile}
-
     metrics = []
     for key in dict.keys():
         for metric in key.split(', '):
@@ -160,37 +132,57 @@ def getStats(dict):
         for index, row in df.iterrows():
             stats = stats.append(row[units.split(', ')], ignore_index=True)
     stats = stats.agg(['mean', 'count', 'std']).transpose()
-    ci95_hi = []
-    ci95_lo = []
+    ci90_hi = []
+    ci90_lo = []
     for i in stats.index:
         m, c, s = stats.loc[i]
-        ci95_hi.append(m + 1.95*s/math.sqrt(c))
-        ci95_lo.append(m - 1.95*s/math.sqrt(c))
-    stats['ci95_hi'] = ci95_hi
-    stats['ci95_lo'] = ci95_lo
+        ci90_hi.append(m + 1.64*s/math.sqrt(c))#1.96 for 95% confidence
+        ci90_lo.append(m - 1.64*s/math.sqrt(c))
+    stats['ci90_hi'] = ci90_hi
+    stats['ci90_lo'] = ci90_lo
     return stats
 
+#Round dateTime to closest day
+def getDateAvg(units, df):
+    data = df
+    data['Datetime'] = pd.to_datetime(data.loc[:, 'date'], format='%Y%m%d%H%M', errors='ignore')
+    data['Datetime'] = data['Datetime'].apply(lambda dt: datetime.datetime(dt.year, dt.month, dt.day))
+    data = data.drop(['date'], axis=1)
+    #data = data.set_index('Datetime')
+    #data = data.groupby(data.index.date).mean()
+    return data
+
+print('Populating DataFrames')
 d1, d2 = populateDataFrames()
-print(d1)
-print(d2)
 
 #userNeeded(d1, 16)
 
-logging.basicConfig(filename='logfile' + str(date.today()) + '.log', level=logging.INFO, format='%(message)s')
+#STATISTICS
+print('Calculating Statistics')
+logging.basicConfig(filename='logfile' + str(datetime.date.today()) + '.log', level=logging.INFO, format='%(message)s')
 logging.info('Original')
 logging.info('Statistics')
-logging.info(getStats({'timeDiff': d1[d1['changeSaved']], 'changeSaved, clicks, OverviewClicks, AddressClicks, ContactClicks, IndividualClicks, TransactionClicks, PurchasesClicks, RewardsClicks, OrdersClicks, InteractionsClicks, ProfileClicks, ClicksInOverview, ClicksInAddress, ClicksInContact, ClicksInIndividual, ClicksInTransaction, ClicksInPurchases, ClicksInRewards, ClicksInOrders, ClicksInInteractions, ClicksInProfile': d1}))
-#logging.info('Dickey-Fuller Test')
-#dickeyfuller(d1)
+logging.info(getStats({'timeDiff': d1[d1['changeSaved']], 'changeSaved, clicks, errandTimeDiff, OverviewClicks, AddressClicks, ContactClicks, IndividualClicks, TransactionClicks, PurchasesClicks, RewardsClicks, OrdersClicks, InteractionsClicks, ProfileClicks, ClicksInOverview, ClicksInAddress, ClicksInContact, ClicksInIndividual, ClicksInTransaction, ClicksInPurchases, ClicksInRewards, ClicksInOrders, ClicksInInteractions, ClicksInProfile, lastOverview, lastAddress, lastContact, lastIndividual, lastTransaction, lastPurchases, lastRewards, lastOrders, lastInteractions, lastProfile': d1.rename(columns = {'timeDiff': 'errandTimeDiff'}, inplace = False)}))
+logging.info('Dickey-Fuller Test')
+dickeyfuller(d1)
+
 logging.info('Treatment')
 logging.info('Statistics')
-logging.info(getStats({'timeDiff': d2[d2['changeSaved']], 'changeSaved, clicks, OverviewClicks, AddressClicks, ContactClicks, IndividualClicks, TransactionClicks, PurchasesClicks, RewardsClicks, OrdersClicks, InteractionsClicks, ProfileClicks, ClicksInOverview, ClicksInAddress, ClicksInContact, ClicksInIndividual, ClicksInTransaction, ClicksInPurchases, ClicksInRewards, ClicksInOrders, ClicksInInteractions, ClicksInProfile': d2}))
-#logging.info('Dickey-Fuller Test')
-#dickeyfuller(d2)
+logging.info(getStats({'timeDiff': d2[d2['changeSaved']], 'changeSaved, clicks, errandTimeDiff, OverviewClicks, AddressClicks, ContactClicks, IndividualClicks, TransactionClicks, PurchasesClicks, RewardsClicks, OrdersClicks, InteractionsClicks, ProfileClicks, ClicksInOverview, ClicksInAddress, ClicksInContact, ClicksInIndividual, ClicksInTransaction, ClicksInPurchases, ClicksInRewards, ClicksInOrders, ClicksInInteractions, ClicksInProfile, lastOverview, lastAddress, lastContact, lastIndividual, lastTransaction, lastPurchases, lastRewards, lastOrders, lastInteractions, lastProfile': d2.rename(columns = {'timeDiff': 'errandTimeDiff'}, inplace = False)}))
+logging.info('Dickey-Fuller Test')
+dickeyfuller(d2)
 
-#graph plot
-d1 = d1.sort_values(by=['date'])#Sort sessions after ascending date
-d1.plot(x='date', y='timeDiff', style = 'o')
+#GRAPH PLOT
+print('Plotting Graphs')
+dateAvg1 = getDateAvg(['timeDiff', 'clicks'], d1.loc[:, ['timeDiff', 'clicks', 'date']])
+dateAvg2 = getDateAvg(['timeDiff', 'clicks'], d2.loc[:, ['timeDiff', 'clicks', 'date']])
+
+fig, ax = plt.subplots(figsize = (15, 7))
+sns.lineplot(ax = ax, x='Datetime', y='clicks', data=dateAvg1, ci = 80).set_title('Average nbr of clicks per errand')
+sns.lineplot(ax = ax, x='Datetime', y='clicks', data=dateAvg2, ci = 80).set_title('Average nbr of clicks per errand')
+
+plt.setp(ax.xaxis.get_majorticklabels(), rotation = 90)#rotate by 90° the labels
+
 plt.show()
 
 #forecastULeffect(d1)
